ea42ca267cc8d499ed3415130b09dd7d90255dff
technocrazepg <pranavgupta0907@gmail.com>
2023-06-27 23:33:15 +0530

    eith commit

--- backend/index.js
+++ backend/index.js
@@ -12,14 +12,14 @@ var corOptions={
 app.use(cors(corOptions));
 // app.use(cors);
 app.use(exp.json());
-// app.use((req,res,next)=>{
-//   res.header("Access-Control-Allow-Origin", req.headers.origin);
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-Width, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-Width, Content-Type, Accept"
+  );
+  next();
+});
 app.use('/auth', require('./routes/auth'));
 app.use('/contact', require('./routes/contact'));
 
