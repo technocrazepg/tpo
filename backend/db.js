3e2da6272ef3287a4bc67cfa1ef5074ff7a369aa
technocrazepg <pranavgupta0907@gmail.com>
2023-06-27 14:19:43 +0530

    commit

--- backend/db.js
+++ backend/db.js
@@ -3,12 +3,16 @@ const dotenv = require("dotenv");
 dotenv.config();
 const URI = process.env.REACT_APP_MONGODB_CONNECTION_STRING
 const connect = async () => {
+  // mongoose.connect(URI,()=>{
+  //   console.log("Db Connect ho gya bhayaji !!");
+  // })
   try{
     mongoose.connect(URI)
-      console.log("Db Connect ho gya bhayaji !!");
-    }
+    console.log("Db Connect ho gya bhayaji !!");
+  }
   catch(err){
     console.log(err);
   }
+  
 };
 module.exports = connect;
